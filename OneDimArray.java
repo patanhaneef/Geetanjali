3a5ee550abe440021ee8393e4c01c90e4e953aef
patanhaneef <149781897+patanhaneef@users.noreply.github.com>
2023-11-03 15:34:04 +0530

    Add files via upload

---
+++ OneDimArray.java
@@ -0,0 +1,13 @@
+class OneDimArray 
+ {
+ public static void main(String args[])
+ {
+ int marks[ ]={10, 30, 50, 60};
+ int l=marks.length; //array length
+ System.out.println("Array elements are");
+ for(int i=0; i<l; i++)
+ {
+ System.out.println(" "+marks[i]);
+ }
+ }
+ }
